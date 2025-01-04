9971568bc3f11c80137b58bcc062216a9ab2e285
Andreo Van Onckelen <andreo141mlg@gmail.com>
2025-01-05 00:10:20 +0100

    separate files, new image, improve paragraphs

---
+++ script.js
@@ -0,0 +1,12 @@
+
+const redirectLink = "https://app.getgrass.io/register/?referralCode=CJhlmZrHFMLP0m1";
+
+// google tag script
+window.dataLayer = window.dataLayer || [];
+function gtag() { dataLayer.push(arguments); }
+gtag('js', new Date());
+gtag('config', 'AW-11536322223');
+
+// copyright msg
+const year = new Date().getFullYear();
+document.getElementById("copyright").innerHTML = `&copy; ${year} Andreo Van Onckelen. All rights reserved.`;
